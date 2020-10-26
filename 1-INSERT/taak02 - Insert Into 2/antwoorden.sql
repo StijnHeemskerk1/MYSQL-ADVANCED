9ddf77c67ec40e6fbaa863c21139831181fc10e4
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-10-26 14:03:40 +0100

    1. taak02 af

--- 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
+++ 1-INSERT/taak02 - Insert Into 2/antwoorden.sql
@@ -1,1 +1,8 @@
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUES ('Jan', 'Vermeer', '2000-08-08', 'Amsterdam')
 
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUES ('Bert', 'Hendriks', '2001-04-28', 'Utrecht')
+
+INSERT INTO studenten (voornaam, achternaam, geboortedatum, woonplaats)
+VALUES ('Marri', 'Janssen', '2003-06-26', 'Almere')
