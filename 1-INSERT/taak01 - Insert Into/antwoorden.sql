1529ec874832f3547529ce5c1c7ada8b08fde167
StijnHeemskerk1 <stijnheemskerk@hotmail.nl>
2020-10-26 13:56:32 +0100

    1. taak01 af

--- 1-INSERT/taak01 - Insert Into/antwoorden.sql
+++ 1-INSERT/taak01 - Insert Into/antwoorden.sql
@@ -1,1 +1,11 @@
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (1, 'Henk', 'van Balen', '9A', 'Albert Cuypstraat 1', '1234AB', '2000-05-18', 'Amsterdam')
 
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (5, 'Piet', 'Pieters', '9A', 'Albert Cuypstraat 5', '1234AB', '2001-04-28', 'Ouderkerk')
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (6, 'Johanna', 'Eduardo', '9C', 'Pieter Aertszstraat 44', '1073CD', '2001-09-02', 'Hoorn')
+
+INSERT INTO studenten (student_id, voornaam, achternaam, klas, adres, postcode, geboortedatum, woonplaats)
+VALUES (7, 'Angela', 'Mohammed', '9C', 'Piet mondriaanplein 14', '1299CD', '2002-05-17', 'Amstelveen')
